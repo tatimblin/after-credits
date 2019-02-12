c8e90b657538b0cb94c6f9bf4f27d31432051f91
Tristan Timblin <tatimblin@gmail.com>
2019-02-12 18:49:30 -0500

    Delete firebaseConfig.js

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCK0MW3FVQFcaitt5KRv1VvHUVTy3-bMG4',
-  authDomain: 'after-credit.firebaseapp.com',
-  databaseURL: 'https://after-credit.firebaseio.com',
-  projectId: 'after-credit',
-  storageBucket: 'after-credit.appspot.com',
-  messagingSenderId: '712777133501',
-};
